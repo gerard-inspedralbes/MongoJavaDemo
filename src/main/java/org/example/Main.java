6016a3da8d69e39d6d06ae4d356982066699a653
gerard-inspedralbes <122524717+gerard-inspedralbes@users.noreply.github.com>
2024-07-12 12:12:04 +0200

    Ïnitial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoIterable;
+
+public class Main {
+    public static void main(String[] args) {
+        // URL de conexión a MongoDB
+        String connectionString = "mongodb+srv://gtorren8:Industex00@cluster0.wm47zyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+        // Crear un cliente de MongoDB
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            // Obtener y listar nombres de las bases de datos
+            MongoIterable<String> databaseNames = mongoClient.listDatabaseNames();
+            for (String dbName : databaseNames) {
+                System.out.println(dbName);
+            }
+        }
+    }
+}
